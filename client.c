4c69082dffdc247d60989b8504f43cbf396eeb12
4416_都甲 真仁 <k15104mt>
2019-04-26 10:24:30 +0900

    20190426_楽しいメッセージ

---
+++ client.c
@@ -0,0 +1,31 @@
+#include<sys/socket.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include<unistd.h>
+#include<string.h>
+
+void commun(int);
+
+int main (int argc, char *argv){
+    int sock = socket(PF_INET,SOCK_STREAM,0);
+
+    struct sockaddr_in target;
+
+    target.sin_family = AF_INET;
+    target.sin_addr.s_addr = inet_addr("10.13.64.20");
+    target.sin_port = htons(10001);
+    //printf("sock is (%d)\n",sock);
+    connect(sock,(struct sockaddr*)&target,sizeof(target));
+	commun(sock);
+    close(sock);
+    return 0;
+}
+
+void commun(int sock){
+	char buf[256];
+	int len_r;
+	char *message="やぁ";
+	send(sock,message,strlen(message),0);
+	len_r = recv(sock,buf,256,0);
+	buf[len_r] = "\0";
+}
