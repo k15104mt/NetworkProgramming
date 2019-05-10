e8ff6b96b5003a71dc62f479d9b0e6f498a45e17
k15104mt <k15104mt@apps.kct.ac.jp>
2019-05-10 10:40:38 +0900

    China

--- client.c
+++ client.c
@@ -1,31 +1,60 @@
 #include<sys/socket.h>
-#include <stdio.h>
-#include <arpa/inet.h>
-#include<unistd.h>
+#include<arpa/inet.h>
+#include<stdio.h>
+#include<unistd.h>//closeを使うため
 #include<string.h>
+#include<stdlib.h>
 
-void commun(int);
+#define BUF_SIZE 256 //マクロ
 
-int main (int argc, char *argv){
-    int sock = socket(PF_INET,SOCK_STREAM,0);
-
-    struct sockaddr_in target;
-
-    target.sin_family = AF_INET;
-    target.sin_addr.s_addr = inet_addr("10.13.64.20");
-    target.sin_port = htons(10001);
-    //printf("sock is (%d)\n",sock);
-    connect(sock,(struct sockaddr*)&target,sizeof(target));
-	commun(sock);
-    close(sock);
-    return 0;
+void DieWithError(char *ErrorMessage){
+	perror(ErrorMessage);//標準エラー出力にエラーメッセージを出す
+	exit(1);//≒終了ステータス。エラーの時は１以上を返す
+	
 }
-
-void commun(int sock){
-	char buf[256];
+void commun(int sock){//原則としてプログラムは上から下に読み込まれるので、どういう関数かを教えるこの関数はmainの上に書いたほうがよい
+	char buf[BUF_SIZE];
 	int len_r;
-	char *message="やぁ";
-	send(sock,message,strlen(message),0);
-	len_r = recv(sock,buf,256,0);
-	buf[len_r] = "\0";
+	char *message = "banana banana banana banana banana banana banana banana banana banana banana";
+	//send(sock,message,strlen(message),0);
+	
+	if(send(sock,message,strlen(message),0)!=strlen(message))DieWithError("send()send a message of unexpected bytes");
+	
+	if(len_r = recv(sock,buf,BUF_SIZE,0)<=0)DieWithError("recv()failed");
+	
+	buf[len_r] = '\0';//nul
+	printf("%s\n",buf);//%s 文字列の出力
 }
+
+
+//send関数の引数　ソケットディスクリプタ、メッセージ、メッセージの文字数、0(固定)
+
+int main(int argc,char**argv){
+	
+	if(argc != 3)DieWithError("arduments is not available");
+	char *server_ipaddr = argv[1];//char *server_ipaddr = "10.13.64.20";
+	int server_port = atoi(argv[2]);//int server_port = 10001;
+	int sock = socket(PF_INET,SOCK_STREAM,0);//通信用の設備を開く
+	if(sock < 0)DieWithError("soclet()failed");
+	
+	struct sockaddr_in target;
+	
+	target.sin_family = AF_INET;
+	
+	//target.sin_addr.s_addr = inet_addr("10.13.64.20");
+	//target.sin_port = htons(10001);
+	
+	target.sin_addr.s_addr = inet_addr("server_ipaddr");
+	target.sin_port = htons(server_port);
+	
+	if(connect(sock,(struct sockaddr*)&target,sizeof(target))<0)DieWithError("connect()failed");
+	
+	commun(sock);
+	//printf("sock is %d",sock);
+	close(sock);	//終了時に閉じる
+
+	return 0;
+}
+
+
+
